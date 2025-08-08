0bf6c4f00d922128842293928469ce0d5966af14
koushik201271 <koushikgoshamy@gmail.com>
2025-08-08 11:01:56 -0600

    index.html

---
+++ script.js
@@ -0,0 +1,11 @@
+const userData = [
+    { username:"koushik", password:"201271" },
+    { username: "arjo",password:"707012" }
+];
+
+
+function checkCredentials (username,password){
+    return new Promise((resolve, reject)) => {
+        setTimeout(())
+    }
+}
