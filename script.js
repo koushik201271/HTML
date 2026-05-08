574780021fbddc6fed5c8da5d17a70b660fe9775
koushik201271 <koushikgoshamy@gmail.com>
2026-05-08 11:47:41 +0600

    index.html

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navItem = document.getElementById("nav-item");
-
-menuToggle.addEventListener("click", () => {
-    navItem.classList.toggle("show");
-    
-    const icon = menuToggle.querySelector("i");
-    icon.classList.toggle("fa-bars");
-    icon.classList.toggle("fa-xmark");
-});
-
-
-document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
+function buyNow(productName) {
+    alert(productName + " has been added to your cart! Our representative will contact you soon.");
+}
